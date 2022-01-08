76ba0acc74377c7218b42d2511449095b7fa3626
iOSDevHeath <iOSDevHeath@icloud.com>
2022-01-08 10:56:15 -0500

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Heath Fashina on 2022-01-08.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
